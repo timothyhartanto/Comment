ef1ef6c41eeda83382b6998b876560124a00b36c
timothyhartanto <hartantotimothy@gmail.com>
2015-12-13 22:45:26 +0700

    Update Login.java

--- Login.java
+++ Login.java
@@ -34,14 +34,9 @@ public class Login extends Activity implements OnClickListener{
 
     //php login script location:
 
-    //localhost :
-    //testing on your device
-    //put your local ip instead,  on windows, run CMD > ipconfig
-    //or in mac's terminal type ifconfig and look for the ip under en0 or en1
-    // private static final String LOGIN_URL = "http://xxx.xxx.x.x:1234/webservice/login.php";
 
     //testing on Emulator:
-    private static final String LOGIN_URL = "http://192.168.13.1:8080/webservice/login.php";
+    private static final String LOGIN_URL = "http://xxx.xxx.xxx.xxx:8080/webservice/login.php";
 
     //testing from a real server:
     //private static final String LOGIN_URL = "http://www.yourdomain.com/webservice/login.php";
